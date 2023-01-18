9c8f004c3b74a1358d94fa93f9cd7b7783ad5286
Timmutegi <mutegi.timothy538@gmail.com>
2023-01-17 22:07:53 -0700

    Updated sign-up component

---
+++ src/app/components/sign-up/sign-up.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss']
+})
+export class SignUpComponent implements OnInit {
+  signupForm = new FormGroup({
+    firstname: new FormControl('', Validators.required),
+    lastname: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+  submitted = false;
+  isLoading!: boolean;
+  fieldTextType!: boolean;
+
+  constructor( ) { }
+
+  ngOnInit(): void {
+  }
+
+  get f() {
+    return this.signupForm.controls;
+  }
+
+  signup() {
+
+  }
+
+}
