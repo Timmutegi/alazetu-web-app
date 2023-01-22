7ef950a3f542d3e006b318ced77ffe9d43cc71d6
Timmutegi <mutegi.timothy538@gmail.com>
2023-01-21 21:21:48 -0700

    Added cart component

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface Product {
+  name: string;
+  cost: string;
+}
+
+const PRODUCT_DATA: Product[] = [
+  {
+    name: "B-flat Ngomabook - Intermediate - 1st Edition",
+    cost: "Ksh 1,490"
+  },
+  {
+    name: "E-flat Ngomabook - Intermediate - 1st Edition",
+    cost: "Ksh 1,490"
+  },
+];
+
 
 @Component({
   selector: 'app-cart',
@@ -6,6 +24,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
+  displayedColumns: string[] = ['name', 'cost'];
+  dataSource = new MatTableDataSource(PRODUCT_DATA);
 
   constructor() { }
 
