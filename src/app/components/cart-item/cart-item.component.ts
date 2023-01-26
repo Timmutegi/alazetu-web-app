5d6a3fa5e6a08f118b276c03b37d2efeed444d8d
Timmutegi <mutegi.timothy538@gmail.com>
2023-01-25 21:11:44 -0700

    Improved cart item component

--- src/app/components/cart-item/cart-item.component.ts
+++ src/app/components/cart-item/cart-item.component.ts
@@ -1,28 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { faMinus } from '@fortawesome/free-solid-svg-icons';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-
 @Component({
   selector: 'app-cart-item',
   templateUrl: './cart-item.component.html',
-  styleUrls: ['./cart-item.component.scss']
+  styleUrls: ['./cart-item.component.scss'],
 })
 export class CartItemComponent {
-  title = 'angularbootstrap';
   inputnumber = 0;
   faPlus = faPlus;
   faMinus = faMinus;
   faTrash = faTrash;
-  
+
+  @Input() name: string = '';
+  @Input() cost: string = '';
+
   plus() {
-   this.inputnumber = this.inputnumber+1;
+    this.inputnumber = this.inputnumber + 1;
   }
 
   minus() {
-    if(this.inputnumber != 0) {
-      this.inputnumber = this.inputnumber-1;
+    if (this.inputnumber != 0) {
+      this.inputnumber = this.inputnumber - 1;
     }
   }
 }
