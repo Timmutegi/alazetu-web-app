043033ea44739c63d537e40c993eace185757f89
Timmutegi <mutegi.timothy538@gmail.com>
2023-01-26 21:33:36 -0700

    Updated product detail component

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faMinus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
+  name: string = "B-flat Ngomabook - Intermediate - 1st Edition";
+  cost: string = "Ksh 1,490";
+  description: string = "An intermediate-level music book for b-flat instruments containing Kenyan songs. Ideal for use by students, teachers and musicians.";
+  inputnumber = 0;
+  faPlus = faPlus;
+  faMinus = faMinus;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  plus() {
+    this.inputnumber = this.inputnumber + 1;
+  }
+
+  minus() {
+    if (this.inputnumber != 0) {
+      this.inputnumber = this.inputnumber - 1;
+    }
   }
 
 }
