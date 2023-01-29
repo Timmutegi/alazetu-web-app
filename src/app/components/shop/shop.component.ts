4e0955dd05b83927a54ddc1317ac515d7bf037dd
Timmutegi <mutegi.timothy538@gmail.com>
2023-01-28 20:58:53 -0700

    Update fake records to have cart field

--- src/app/components/shop/shop.component.ts
+++ src/app/components/shop/shop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-shop',
@@ -6,35 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop.component.scss'],
 })
 export class ShopComponent implements OnInit {
+  faCheck = faCheck;
   products = [
     {
       "name": "B-flat Ngomabook - Intermediate - 1st Edition",
-      "cost": "Ksh 1,490"
+      "cost": "Ksh 1,490",
+      "cart": true
     },
     {
       "name": "E-flat Ngomabook - Intermediate - 1st Edition",
-      "cost": "Ksh 1,490"
+      "cost": "Ksh 1,490",
+      "cart": false
     },
     {
       "name": "Guitar Ngomabook - Intermediate - 1st Edition",
-      "cost": "Ksh 1,490"
+      "cost": "Ksh 1,490",
+      "cart": true
     },
     {
       "name": "Piano Ngomabook - Intermediate - 1st Edition",
-      "cost": "Ksh 1,490"
+      "cost": "Ksh 1,490",
+      "cart": true
     },
     {
       "name": "Violin Ngomabook - Intermediate - 1st Edition",
-      "cost": "Ksh 1,490"
+      "cost": "Ksh 1,490",
+      "cart": false
     },
     {
       "name": "Voice (Soprano, Alto) Ngomabook - Intermediate - 1st Edition",
-      "cost": "Ksh 1,490"
+      "cost": "Ksh 1,490",
+      "cart": true
     },
     
   ];
+cart: any;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  addToCart(): void {
+
+  }
 }
