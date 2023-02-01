760f1a75661de5fcf5d33dbb71ee353401b7e0c0
Timmutegi <mutegi.timothy538@gmail.com>
2023-01-31 23:37:11 -0700

    Added fake user

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -4,7 +4,30 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthenticationService {
-  user = null;
+  fake_user = {
+    logged_in: false,
+    firstname: "Timothy",
+    lastname: "Mbaka",
+    cart: 0
+  }
 
   constructor() { }
+
+  login_user() {
+    this.fake_user = {
+      logged_in: true,
+      firstname: "Timothy",
+      lastname: "Mbaka",
+      cart: 3
+    }
+  }
+
+  logout() {
+    this.fake_user = {
+      logged_in: false,
+      firstname: "Timothy",
+      lastname: "Mbaka",
+      cart: 0
+    };
+  }
 }
