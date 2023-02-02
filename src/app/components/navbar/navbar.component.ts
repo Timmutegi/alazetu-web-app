2bccd07da5c22afc7622b5cefaa1e54422f24f8e
Timmutegi <mutegi.timothy538@gmail.com>
2023-02-01 22:33:32 -0700

    Added cart icon

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/services/authentication.service';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +8,7 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  faCartPlus = faCartShopping;
 
   constructor(
     public auth: AuthenticationService
