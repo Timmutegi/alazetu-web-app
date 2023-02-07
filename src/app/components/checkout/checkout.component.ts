bcecb10df8d361d478aa1082d251cfc194f46bdc
Timmutegi <mutegi.timothy538@gmail.com>
2023-02-06 22:48:13 -0700

    Added dummy products and total calculation

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,24 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+export interface Product {
+  id: number;
+  name: string;
+  cost: number;
+  quantity: number;
+}
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss']
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit {
   isLoading!: boolean;
-  products = [
+  totalPrice = 0;
+  products: Product[] = [
     {
-      "name": "B-flat Ngomabook - Intermediate - 1st Edition",
-      "cost": "Ksh 1,490"
+      id: 1,
+      name: "E-flat Ngomabook - Intermediate - 1st Edition",
+      quantity: 3,
+      cost: 1490
     },
     {
-      "name": "E-flat Ngomabook - Intermediate - 1st Edition",
-      "cost": "Ksh 1,490"
+      id: 3,
+      name: "Piano Ngomabook - Intermediate - 1st Edition",
+      quantity: 3,
+      cost: 1490
+    },
+    {
+      id: 5,
+      name: "Voice (Soprano, Alto) Ngomabook - Intermediate - 1st Edition",
+      cost: 1490,
+      quantity: 6
     }
   ];
+
+  ngOnInit(): void {
+    this.calculateTotal();
+  }
+
+  calculateTotal () {
+    this.totalPrice = this.products.reduce(function (accumulator, item) {
+      return accumulator + item.quantity * item.cost;
+    }, 0);  
+  }
 
   placeOrder() {
     
   }
+
+
 }
