04c43401436c2af7e360f2185addae715c214fe8
Timmutegi <mutegi.timothy538@gmail.com>
2023-02-10 20:45:35 -0700

    Implemented cart total observable

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -9,20 +10,25 @@ import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 })
 export class NavbarComponent implements OnInit {
   faCartPlus = faCartShopping;
+  cartItems = 0;
+  private subscription!: Subscription;
 
   constructor(
     private auth: AuthenticationService
   ) { }
 
   ngOnInit(): void {
+    this.subscription = this.auth.cartTotal$.subscribe(total => {
+      this.cartItems = total;
+    });  
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   logout(): void {
     this.auth.logout();
-  }
-
-  cartItems(): Number {
-    return this.auth.fake_user.cart.length;
   }
 
   isLoggedIn(): boolean {
