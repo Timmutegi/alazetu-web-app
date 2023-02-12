fbb97f623c9a6754e1f7e4a44421e8ad1961fe17
Timmutegi <mutegi.timothy538@gmail.com>
2023-02-11 22:45:57 -0700

    Implemented cart products logic

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -15,11 +15,25 @@ export interface Product {
 })
 export class CartComponent implements OnInit {
   totalPrice = 0;
+  cartItems: number[]  = [];
+  selectedProducts: Product [] = [];
   products: Product[] = [
+    {
+      id: 0,
+      name: "B-flat Ngomabook - Intermediate - 1st Edition",
+      quantity: 2,
+      cost: 1490
+    },
     {
       id: 1,
       name: "E-flat Ngomabook - Intermediate - 1st Edition",
       quantity: 3,
+      cost: 1490
+    },
+    {
+      id: 2,
+      name: "Guitar Ngomabook - Intermediate - 1st Edition",
+      quantity: 2,
       cost: 1490
     },
     {
@@ -29,11 +43,17 @@ export class CartComponent implements OnInit {
       cost: 1490
     },
     {
+      id: 4,
+      name: "Violin Ngomabook - Intermediate - 1st Edition",
+      cost: 1490,
+      quantity: 5
+    },
+    {
       id: 5,
       name: "Voice (Soprano, Alto) Ngomabook - Intermediate - 1st Edition",
       cost: 1490,
       quantity: 6
-    }
+    },
   ];
 
   constructor(
@@ -41,23 +61,25 @@ export class CartComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.cartItems = this.auth.fake_user.cart;
+    this.selectedProducts = this.products.filter(item => this.cartItems.includes(item.id));
     this.calculateTotal();
   }
 
   updateProductQuantity(data: any) {
-    const target_product = this.products.findIndex((product => product.id == data.id));
-    this.products[target_product].quantity = data.quantity;
+    const target_product = this.selectedProducts.findIndex((product => product.id == data.id));
+    this.selectedProducts[target_product].quantity = data.quantity;
     this.calculateTotal();
   }
 
   calculateTotal () {
-    this.totalPrice = this.products.reduce(function (accumulator, item) {
+    this.totalPrice = this.selectedProducts.reduce(function (accumulator, item) {
       return accumulator + item.quantity * item.cost;
     }, 0);  
   }
 
   removeCartItem(data: any) {
-    this.products = this.products.filter(product => product.id != data.id);
+    this.selectedProducts = this.selectedProducts.filter(product => product.id != data.id);
     this.calculateTotal();
     this.auth.removeItemFromCart(data.id);
   }
