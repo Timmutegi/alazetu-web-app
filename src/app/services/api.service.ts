95de91d67fd6305783286ecf30cbe014ef7e26ea
Timmutegi <mutegi.timothy538@gmail.com>
2023-02-12 22:32:27 -0700

    Added REST api base url and methods

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  baseUrl = 'https://alazetunodejsbackend.onrender.com/api';
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  get(endpoint: string) {
+    return this.http.get<any>(this.baseUrl + endpoint);
+  }
+
+  post(endpoint: string, data: JSON) {
+    return this.http.post<any>(this.baseUrl + endpoint, data);
+  }
+
+  patch(endpoint: string, data: JSON) {
+    return this.http.patch<any>(this.baseUrl + endpoint, data);
+  }
 }
