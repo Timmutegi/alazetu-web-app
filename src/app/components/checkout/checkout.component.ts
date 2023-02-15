3f869e7b555d2b380916c212a68a1685bf03b00d
Timmutegi <mutegi.timothy538@gmail.com>
2023-02-14 22:10:12 -0700

    Add cart items to checkout

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
-export interface Product {
-  id: number;
-  name: string;
-  cost: number;
-  quantity: number;
-}
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Product } from 'src/app/shared/product';
 
 @Component({
   selector: 'app-checkout',
@@ -15,11 +10,25 @@ export interface Product {
 export class CheckoutComponent implements OnInit {
   isLoading!: boolean;
   totalPrice = 0;
+  cartItems: number[]  = [];
+  selectedProducts: Product [] = [];
   products: Product[] = [
+    {
+      id: 0,
+      name: "B-flat Ngomabook - Intermediate - 1st Edition",
+      quantity: 2,
+      cost: 1490
+    },
     {
       id: 1,
       name: "E-flat Ngomabook - Intermediate - 1st Edition",
       quantity: 3,
+      cost: 1490
+    },
+    {
+      id: 2,
+      name: "Guitar Ngomabook - Intermediate - 1st Edition",
+      quantity: 2,
       cost: 1490
     },
     {
@@ -29,19 +38,31 @@ export class CheckoutComponent implements OnInit {
       cost: 1490
     },
     {
+      id: 4,
+      name: "Violin Ngomabook - Intermediate - 1st Edition",
+      cost: 1490,
+      quantity: 5
+    },
+    {
       id: 5,
       name: "Voice (Soprano, Alto) Ngomabook - Intermediate - 1st Edition",
       cost: 1490,
       quantity: 6
-    }
+    },
   ];
 
+  constructor(
+    private auth: AuthenticationService
+  ) { }
+
   ngOnInit(): void {
+    this.cartItems = this.auth.fake_user.cart;
+    this.selectedProducts = this.products.filter(item => this.cartItems.includes(item.id));
     this.calculateTotal();
   }
 
   calculateTotal () {
-    this.totalPrice = this.products.reduce(function (accumulator, item) {
+    this.totalPrice = this.selectedProducts.reduce(function (accumulator, item) {
       return accumulator + item.quantity * item.cost;
     }, 0);  
   }
