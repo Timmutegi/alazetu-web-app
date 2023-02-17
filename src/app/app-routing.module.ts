594073a6c0582f7cc208ef1540d944067725fa7c
Timmutegi <mutegi.timothy538@gmail.com>
2023-02-16 21:35:14 -0700

    Refactored routes to shared module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './auth/auth.guard';
-import { AboutComponent } from './components/about/about.component';
-import { AccountComponent } from './components/account/account.component';
-import { CartComponent } from './components/cart/cart.component';
-import { CheckoutComponent } from './components/checkout/checkout.component';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { ManageAccountComponent } from './components/manage-account/manage-account.component';
-import { ProductDetailComponent } from './components/product-detail/product-detail.component';
-import { ShopComponent } from './components/shop/shop.component';
-import { SignUpComponent } from './components/sign-up/sign-up.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'shop', component: ShopComponent },
-  { path: 'about-us', component: AboutComponent },
-  { path: 'my-account', component: AccountComponent, canActivate: [AuthGuard] },
-  { path: 'create-account', component: SignUpComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'cart', component: CartComponent, canActivate: [AuthGuard]},
-  { path: 'checkout', component: CheckoutComponent, canActivate: [AuthGuard] },
-  { path: 'forgot-password', component: ForgotPasswordComponent },
-  { path: 'manage-account', component: ManageAccountComponent, canActivate: [AuthGuard] },
-  { path: 'product/:ID', component: ProductDetailComponent },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
