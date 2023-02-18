173b9d3875af7501a6a96df1804338fb457448d4
Timmutegi <mutegi.timothy538@gmail.com>
2023-02-17 21:00:22 -0700

    Added login method and method with auth token

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -15,11 +16,21 @@ export class ApiService {
     return this.http.get<any>(this.baseUrl + endpoint);
   }
 
+  login(endpoint: string, data: JSON): Observable<string> {
+    return this.http.post(this.baseUrl + endpoint, data, { responseType: 'text' });
+  }
+
   post(endpoint: string, data: JSON) {
     return this.http.post<any>(this.baseUrl + endpoint, data);
   }
 
   patch(endpoint: string, data: JSON) {
     return this.http.patch<any>(this.baseUrl + endpoint, data);
+  }
+
+  getwithAuth(endpoint: string): Observable<any> {
+    const authToken = localStorage.getItem('authToken') ?? '';
+    const headers = new HttpHeaders().set('X-Auth-Token', authToken);
+    return this.http.get(this.baseUrl + endpoint, { headers });
   }
 }
