e77957dd8e119d22fb75df3935f9a89c10b1a35f
Timmutegi <mutegi.timothy538@gmail.com>
2023-02-17 21:05:29 -0700

    Refactored to modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatTableModule } from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
+
+import { SharedModule } from 'src/app/modules/shared/shared.module';
+import { AdminModule } from 'src/app/modules/admin/admin.module';
+import { AuthenticationModule } from 'src/app/modules/authentication/authentication.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ShopComponent } from './components/shop/shop.component';
 import { AboutComponent } from './components/about/about.component';
 import { AccountComponent } from './components/account/account.component';
-import { LoginComponent } from './components/login/login.component';
-import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { CartComponent } from './components/cart/cart.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { CartItemComponent } from './components/cart-item/cart-item.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ManageAccountComponent } from './components/manage-account/manage-account.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent,
     FooterComponent,
     ShopComponent,
     AboutComponent,
     AccountComponent,
-    LoginComponent,
-    SignUpComponent,
     CartComponent,
     ProductDetailComponent,
     CartItemComponent,
@@ -44,12 +42,15 @@ import { ForgotPasswordComponent } from './components/forgot-password/forgot-pas
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
     MatTableModule,
     BrowserAnimationsModule,
-    FormsModule,
+    HttpClientModule,
+    SharedModule,
+    AuthenticationModule,
+    AdminModule,
     FontAwesomeModule,
-    HttpClientModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
